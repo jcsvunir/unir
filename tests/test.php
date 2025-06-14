9b57cd9d006a0e13e0b240274ec1126e4f80fb97
Juan Carlos <juan.carlos.serrano@easym2m.eu>
2025-06-14 08:37:57 +0200

    Todo bien

--- tests/test.php
+++ tests/test.php
@@ -3,9 +3,9 @@
 include_once __DIR__ . DIRECTORY_SEPARATOR . 'SDK' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'SDK' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'omv1_boostrap.php';
 
-$options['idCustomer'] = "c5dcc14d5398585.34505544";
-$options['year'] = "2022";
-$options['month'] = "05";
+$options['idCustomer'] = "<customer_id>"; // ID del cliente
+$options['year'] = "<year>"; // Año de la factura
+$options['month'] = "<mes>"; // Mes de la factura
 
 try {
     // Crear objeto factura
@@ -34,18 +34,11 @@ try {
 
     } else {
         $invoiceArray = $invoice->getInvoiceArray();
-        //print_r($invoiceArray);
-        // Save invoice on DB, return invoiceID
-        //$invoiceId = $invoice->save(true);
-        // Save invoice on GCS in CSV format
-        // Step#1: get CSV on string format from invoice in array format.
+
         $csvString = $invoice->__toCSV();
 
         print_r($csvString);
-        echo "\n";
-        // Step#2: save CSV string on GCS.
-        //$invoice->saveCSV($csvString,true);
-        // Verify if exists CSV on GCS
+
         if ($invoice->existsCSV()) {
             echo "\nExists CSV file for this invoice stored on GCS.\n";
         }
